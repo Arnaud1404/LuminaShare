c41529035a3dc897e03028c839ed34324dc2d12a
Arnaud WSL <arnaudgomes14@gmail.com>
2025-02-21 18:51:16 +0100

    Fixed imageprocessing package

--- backend/src/main/java/pdl/imageprocessing/GrayLevelProcessing.java
+++ backend/src/main/java/pdl/imageprocessing/GrayLevelProcessing.java
@@ -1,8 +1,7 @@
-package imageprocessing;
+package pdl.imageprocessing;
 
 import boofcv.io.image.UtilImageIO;
 import boofcv.struct.image.GrayU8;
-
 
 public class GrayLevelProcessing {
 
@@ -19,14 +18,15 @@ public class GrayLevelProcessing {
 			}
 		}
 	}
-	public static void modif_lum (GrayU8 input, int delta){
+
+	public static void modif_lum(GrayU8 input, int delta) {
 		for (int y = 0; y < input.height; ++y) {
 			for (int x = 0; x < input.width; ++x) {
 				int gl = input.get(x, y);
 				gl = gl + delta;
-				if(gl<0)
+				if (gl < 0)
 					gl = 0;
-				if (gl > 255){
+				if (gl > 255) {
 					gl = 255;
 				}
 				input.set(x, y, gl);
@@ -34,7 +34,7 @@ public class GrayLevelProcessing {
 		}
 	}
 
-	public static void etend_dynamic (GrayU8 input){
+	public static void etend_dynamic(GrayU8 input) {
 		int LUT[] = new int[256];
 
 		int min = 256;
@@ -42,56 +42,56 @@ public class GrayLevelProcessing {
 		int gl = 0;
 		for (int y = 0; y < input.height; ++y) {
 			for (int x = 0; x < input.width; ++x) {
-				gl = input.get(x,y);
+				gl = input.get(x, y);
 				if (gl < min)
 					min = gl;
-				if( gl > max)
+				if (gl > max)
 					max = gl;
 			}
 		}
 
-		for(int i=0; i<256; i=i+1){
-			LUT[i] = 255 * (i-min) / (max-min) ;
+		for (int i = 0; i < 256; i = i + 1) {
+			LUT[i] = 255 * (i - min) / (max - min);
 		}
 
 		for (int y = 0; y < input.height; ++y) {
 			for (int x = 0; x < input.width; ++x) {
-				gl = input.get(x,y);
+				gl = input.get(x, y);
 				gl = LUT[gl];
-				input.set(x,y,gl);
+				input.set(x, y, gl);
 			}
 		}
 
 	}
 
-public static void etend_dynamic_pas_opt (GrayU8 input){
+	public static void etend_dynamic_pas_opt(GrayU8 input) {
 		int min = 256;
 		int max = 0;
 		int gl = 0;
 
 		for (int y = 0; y < input.height; ++y) {
 			for (int x = 0; x < input.width; ++x) {
-				gl = input.get(x,y);
+				gl = input.get(x, y);
 				if (gl < min)
 					min = gl;
-				if( gl > max)
+				if (gl > max)
 					max = gl;
 			}
 		}
 
 		for (int y = 0; y < input.height; ++y) {
 			for (int x = 0; x < input.width; ++x) {
-				gl = input.get(x,y);
-				gl = 255 * (gl-min) / (max-min);
-				input.set(x,y,gl);
+				gl = input.get(x, y);
+				gl = 255 * (gl - min) / (max - min);
+				input.set(x, y, gl);
 			}
 		}
 
 	}
 
-	public static int[] histogram(GrayU8 input){
+	public static int[] histogram(GrayU8 input) {
 		int histogram[] = new int[256];
-		int gl=0;
+		int gl = 0;
 		for (int y = 0; y < input.height; ++y) {
 			for (int x = 0; x < input.width; ++x) {
 				gl = input.get(x, y);
@@ -100,17 +100,18 @@ public static void etend_dynamic_pas_opt (GrayU8 input){
 		}
 		return histogram;
 	}
-	public static void egalisation_histo(GrayU8 input){
+
+	public static void egalisation_histo(GrayU8 input) {
 		int histogram[] = histogram(input);
 		int histogram_egalise[] = new int[256];
 		int LUT[] = new int[256];
-		int nb_pixel = 0 ;
+		int nb_pixel = 0;
 		int nb_gl = 0;
 		int min = 256;
 		int max = 0;
 		int gl;
 
-		for(int i =0; i<256; i=i+1){
+		for (int i = 0; i < 256; i = i + 1) {
 			if (min == 0 && histogram[i] != 0)
 				min = histogram[i];
 			nb_gl = histogram[i];
@@ -118,49 +119,47 @@ public static void etend_dynamic_pas_opt (GrayU8 input){
 			histogram_egalise[i] = nb_pixel;
 		}
 
-
 		for (int y = 0; y < input.height; ++y) {
 			for (int x = 0; x < input.width; ++x) {
-				gl = input.get(x,y);
+				gl = input.get(x, y);
 				if (gl < min)
 					min = gl;
-				if( gl > max)
+				if (gl > max)
 					max = gl;
 			}
 		}
 
 		for (int y = 0; y < input.height; ++y) {
 			for (int x = 0; x < input.width; ++x) {
-				gl = input.get(x,y);
-				gl = histogram_egalise[gl]*255/nb_pixel;
-				input.set(x,y,gl);
+				gl = input.get(x, y);
+				gl = histogram_egalise[gl] * 255 / nb_pixel;
+				input.set(x, y, gl);
 			}
 		}
 
 	}
 
+	public static void main(String[] args) {
 
-    public static void main( String[] args ) {
-
-    	// load image
+		// load image
 		if (args.length < 2) {
 			System.out.println("missing input or output image filename");
 			System.exit(-1);
 		}
 		final String inputPath = args[0];
 		GrayU8 input = UtilImageIO.loadImage(inputPath, GrayU8.class);
-		if(input == null) {
+		if (input == null) {
 			System.err.println("Cannot read input file '" + inputPath);
 			System.exit(-1);
 		}
 
 		// processing
-		
-        // threshold(input, 128);
+
+		// threshold(input, 128);
 		// modif_lum(input,-50);
 		etend_dynamic_pas_opt(input);
 		// egalisation_histo(input);
-		
+
 		// save output image
 		final String outputPath = args[1];
 		UtilImageIO.saveImage(input, outputPath);
