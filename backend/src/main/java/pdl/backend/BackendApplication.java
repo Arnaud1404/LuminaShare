6738ab6a07034a14cc7dec4b3f97ba53f1119074
Soraya <soraya.benachour@etu.u-bordeaux.fr>
2025-03-03 12:47:17 +0100

    initialisation #11

--- backend/src/main/java/pdl/backend/BackendApplication.java
+++ backend/src/main/java/pdl/backend/BackendApplication.java
@@ -10,28 +10,6 @@ import java.io.File;
 
 @SpringBootApplication
 public class BackendApplication {
-
-@Component
-public class ImageInitializer implements CommandLineRunner {
-
-    private static final String IMAGE_FOLDER = "backend/src/main/resources/images"; 
-
-    @Autowired
-    private ImageService imageService; // Service pour charger les images
-
-    @Override
-    public void run(String... args) throws Exception {
-        File folder = new File(IMAGE_FOLDER);
-
-        // Si le dossier n'existe pas, lever une exception
-        if (!folder.exists() || !folder.isDirectory()) {
-            throw new RuntimeException("Le dossier " + IMAGE_FOLDER + " est introuvable. Veuillez le créer.");
-        }
-
-        // Charger les images dans la base de données
-        imageService.loadImagesFromDirectory(IMAGE_FOLDER);
-    }
-}
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
 	}
