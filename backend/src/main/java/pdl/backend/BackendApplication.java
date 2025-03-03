5404d9eac9059e9e3a0521f68f04c7dddf3e17cb
Soraya <soraya.benachour@etu.u-bordeaux.fr>
2025-03-03 12:50:26 +0100

    initialisation #11

--- backend/src/main/java/pdl/backend/BackendApplication.java
+++ backend/src/main/java/pdl/backend/BackendApplication.java
@@ -3,10 +3,7 @@ package pdl.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.stereotype.Component;
-import org.springframework.beans.factory.annotation.Autowired;
-import java.io.File;
+
 
 @SpringBootApplication
 public class BackendApplication {
