7316d05f71e8cfbf46a29f233e2d7d26508fd19e
Arnaud WSL <arnaudgomes14@gmail.com>
2025-03-07 15:12:24 +0100

    Issue9 - Supprimer image depuis le frontend + reformatage

--- frontend/src/components/Gallery.vue
+++ frontend/src/components/Gallery.vue
@@ -1,10 +1,6 @@
 <script setup lang="ts">
+import {images} from './images';
 
-import type { ImageGallery } from "./http-api"
-
-defineProps<{
-  images: ImageGallery[]
-}>()
 </script>
 
 <template>
