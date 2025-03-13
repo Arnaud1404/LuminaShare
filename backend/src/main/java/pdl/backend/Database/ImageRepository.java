dac98a9f452d4efdc66eb5848ec7b652c31f38df
Arnaud WSL <arnaudgomes14@gmail.com>
2025-03-13 16:47:21 +0100

    Added hueSat column in database

--- backend/src/main/java/pdl/backend/Database/ImageRepository.java
+++ backend/src/main/java/pdl/backend/Database/ImageRepository.java
@@ -61,7 +61,7 @@ public class ImageRepository implements InitializingBean {
         this.jdbcTemplate
                 .execute(
                         "CREATE TABLE IF NOT EXISTS " + databaseTable
-                                + " (id bigserial PRIMARY KEY, name character varying(255) UNIQUE, type character varying(10), size character varying(255), rgbcube vector(512))");
+                                + " (id bigserial PRIMARY KEY, name character varying(255) UNIQUE, type character varying(10), size character varying(255), rgbcube vector(512), hueSat vector(101))");
     }
 
     /**
@@ -89,7 +89,7 @@ public class ImageRepository implements InitializingBean {
     }
 
     /**
-     * Adds an image to the database with their descriptors
+     * Adds an image to the database with its descriptors
      * 
      * @param image image to add to the database
      * @return 1 if the image was added, else 0
@@ -100,11 +100,13 @@ public class ImageRepository implements InitializingBean {
                 return 0;
             }
 
-            PGvector histogram = createHistogramFromImage(img);
-            if (histogram == null) {
+            PGvector rgbcube = createRgbHistogramFromImage(img);
+            PGvector hueSat = createHueSaturationHistogramFromImage(img);
+
+            if (rgbcube == null || hueSat == null) {
                 return 0;
             }
-            return insertImageRecord(img, histogram);
+            return insertImageRecord(img, rgbcube, hueSat);
         } catch (Exception e) {
             System.err.println("Error adding image to database: " + e.getMessage());
             return 0;
@@ -128,7 +130,7 @@ public class ImageRepository implements InitializingBean {
      */
     public Image getById(long id) {
         try {
-            String sql = "SELECT id, name, type, size, rgbcube FROM " + databaseTable + " WHERE id = ?";
+            String sql = "SELECT id, name, type, size, rgbcube, hueSat FROM " + databaseTable + " WHERE id = ?";
             return jdbcTemplate.queryForObject(sql, Image.class, id);
         } catch (Exception e) {
             return null; // Non trouvé
@@ -172,9 +174,13 @@ public class ImageRepository implements InitializingBean {
     }
 
     /**
-     * Creates a histogram vector from an image
+     * Creates a 3D RGB histogram vector from an image
+     *
+     * @param img The image object containing metadata and path information
+     * @return PGvector containing the hue-saturation histogram, or null if
+     *         processing fails
      */
-    private PGvector createHistogramFromImage(Image img) {
+    private PGvector createRgbHistogramFromImage(Image img) {
         try {
             BufferedImage input = UtilImageIO.loadImage(img.getPath() + "/" + img.getName());
             if (input == null) {
@@ -184,6 +190,29 @@ public class ImageRepository implements InitializingBean {
 
             Planar<GrayU8> image = ConvertBufferedImage.convertFromPlanar(input, null, true, GrayU8.class);
             return ImagePGVector.createRgb(image, 8);
+        } catch (Exception e) {
+            System.err.println("Failed to create histogram: " + e.getMessage());
+            return null;
+        }
+    }
+
+    /**
+     * Creates a hue-saturation histogram vector from an image
+     *
+     * @param img The image object containing metadata and path information
+     * @return PGvector containing the hue-saturation histogram, or null if
+     *         processing fails
+     */
+    private PGvector createHueSaturationHistogramFromImage(Image img) {
+        try {
+            BufferedImage input = UtilImageIO.loadImage(img.getPath() + "/" + img.getName());
+            if (input == null) {
+                System.err.println("Could not load image: " + img.getPath() + "/" + img.getName());
+                return null;
+            }
+
+            Planar<GrayU8> image = ConvertBufferedImage.convertFromPlanar(input, null, true, GrayU8.class);
+            return ImagePGVector.createHueSaturation(image);
         } catch (Exception e) {
             System.err.println("Failed to create histogram: " + e.getMessage());
             return null;
@@ -210,16 +239,19 @@ public class ImageRepository implements InitializingBean {
      *                insert
      * @param rgbcube The PGvector containing the RGB histogram data for
      *                similarity search
+     * @param hueSat  The PGvector containing the Hue-Saturation histogram
+     *                data for similarity search
      * @return 1 if insertion was successful, 0 if it failed
      */
-    private int insertImageRecord(Image img, PGvector rgbcube) {
+    private int insertImageRecord(Image img, PGvector rgbcube, PGvector hueSat) {
         try {
             jdbcTemplate.update(
-                    "INSERT INTO " + databaseTable + " (name, type, size, rgbcube) VALUES (?, ?, ?, ?)",
+                    "INSERT INTO " + databaseTable + " (name, type, size, rgbcube, hueSat) VALUES (?, ?, ?, ?, ?)",
                     img.getName(),
                     img.getType().toString(),
                     img.getSize(),
-                    rgbcube);
+                    rgbcube,
+                    hueSat);
             return 1;
         } catch (Exception e) {
             System.err.println("Database insertion failed: " + e.getMessage());
