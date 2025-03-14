2780a39914f1049f30b8bb3479021eac52677e66
Arnaud <arnaudgomes14@gmail.com>
2025-03-14 13:01:11 +0100

    imagedao doc

--- backend/src/main/java/pdl/backend/ImageDao.java
+++ backend/src/main/java/pdl/backend/ImageDao.java
@@ -28,10 +28,14 @@ public class ImageDao implements Dao<Image> {
 
   private final Map<Long, Image> images = new HashMap<>();
 
-  public long getImageCount() {
-    return images.size();
-  }
-
+  /**
+   * Saves an image to the in-memory collection.
+   * Processes the raw file data to extract image metadata.
+   * 
+   * @param fileName    Name of the image file
+   * @param fileContent Raw byte array of the image file
+   * @throws RuntimeException if image processing fails
+   */
   public Image saveImage(String fileName, byte[] fileContent) {
     try {
       BufferedImage bufferedImage = ImageIO.read(new ByteArrayInputStream(fileContent));
@@ -52,29 +56,64 @@ public class ImageDao implements Dao<Image> {
     }
   }
 
+  /**
+   * Retrieves a single image from memory by its id
+   * 
+   * @param id The unique id of the image to retrieve
+   * @return Optional containing the image if found, else empty Optional
+   */
   @Override
   public Optional<Image> retrieve(final long id) {
     Optional<Image> img = Optional.ofNullable(images.get(id));
     return img;
   }
 
+  /**
+   * Retrieves all images from the in-memory collection
+   * 
+   * @return List of all images in memory
+   */
   @Override
   public List<Image> retrieveAll() {
     return new ArrayList<Image>(images.values());
   }
 
+  /**
+   * Adds a new image to the in-memory collection
+   * 
+   * @param img The image to add to the collection
+   */
   @Override
   public void create(final Image img) {
     images.put(img.getId(), img);
   }
 
+  /**
+   * Deletes an image from the in-memory collection
+   * 
+   * @param img The image to remove
+   */
   @Override
   public void update(final Image img, final String[] params) {
     // Not used
   }
 
+  /**
+   * Deletes an image from the in-memory collection
+   * 
+   * @param img The image to remove
+   */
   @Override
   public void delete(final Image img) {
     images.remove(img.getId());
+  }
+
+  /**
+   * Gets the number of images in the in-memory collection
+   * 
+   * @return The count of images in memory
+   */
+  public int getImageCount() {
+    return images.size();
   }
 }
